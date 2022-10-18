27d3db012b16ffa4ed5a60d1bb2b8d22757dec71
Ebenezer <ebenezerdorneles@gmail.com>
2022-10-18 19:33:46 -0300

    feat: add variables to index development

---
+++ script/methods/development_index.R
@@ -0,0 +1,5 @@
+# gross development index -------------------------------------------------
+
+factor_score <- as.data.frame(fa$scores)
+
+factor_characteristic_root_factors <- as.data.frame(fa$Vaccounted)[1, 1:5]
