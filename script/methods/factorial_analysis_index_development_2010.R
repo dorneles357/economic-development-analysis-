2fde115b729ff78d7ba8c6cdd28de0a6b533d38f
Ebenezer <ebenezerdorneles@gmail.com>
2022-10-30 13:30:46 -0300

    fix: adjust fa method

--- script/methods/factorial_analysis_index_development_2010.R
+++ script/methods/factorial_analysis_index_development_2010.R
@@ -4,12 +4,12 @@ fa_SC <- read_delim("data/data-2010.csv",
                       delim = ";", escape_double = FALSE, locale = locale(decimal_mark = ","), 
                       trim_ws = TRUE)%>%
   rename(index =`...1`)%>%
-  select(-code_muni, -index, -x1, -x10)%>%
+  select(-code_muni, -index, -x1, -x10, -x5, -x6, -x9, -x18)%>%
   janitor::clean_names()
 
 # analysis ----------------------------------------------------------------
 
-fa <- fa(fa_SC[2:17], nfactors = 2, rotate = "varimax", scores = "regression")
+fa <- fa(fa_SC[2:13], nfactors = 3, rotate = "varimax", scores = "regression")
 
 fa <- na.omit(fa)
 
@@ -19,9 +19,9 @@ fa <- na.omit(fa)
 
 # gross development index -------------------------------------------------
 
-factor_score <- as.matrix(fa$scores[1:106, 1:5])
+factor_score <- as.matrix(fa$scores[1:106, 1:3])
 
-characteristic_root <- as.matrix(fa$Vaccounted[1, 1:5])
+characteristic_root <- as.matrix(fa$Vaccounted[1, 1:3])
 
 index = (factor_score %*% characteristic_root) /  sum(characteristic_root)
 
